37c0264772cde2416f0a07512be668443f650e6c
lucas4543tho <56897828+lucas4543tho@users.noreply.github.com>
2019-10-29 11:37:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://pestssolution.us/nor/index.html");
+header("Location: https://www.mcafee.com/consumer/en-us/store/m0/catalog/mls_430/mcafee-livesafe.html");
 die();
 ?>
