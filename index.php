7c9fcf0832745da1f17e8889441cd924b503d1a3
lucas4543tho <56897828+lucas4543tho@users.noreply.github.com>
2020-04-30 20:03:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://card.americanexpress.com/d/green-card-compare/?utm_mcid=3560620&utm_source=bing&utm_medium=cpc&utm_term=american+express&utm_campaign=p%3AB%7Ca%3AAXCB%7C%5EB%7C%23D%7Cd%3AAmerican+Express+TP%7Cm%3AExact%7Cg%3ANA%7Csid%3A10449%7Cmcid%3A3560620&utm_adgroup=American+Express%7Cp%3AB%7Ca%3AAXCB%7C%5EB%7C%23D%7Cd%3AAmerican+Express+TP%7Cm%3AExact%7Cg%3ANA%7Csid%3A10449%7Cmcid%3A3560620&utm_cmpid=73172647&utm_adgid=4805441420&utm_tgtid=kwd-27137902008%3Aloc-190&utm_mt=be&utm_adid=16482097765&utm_dvc=c&utm_ntwk=o&utm_programname=brandtp&msclkid=5c49c9b7b6121724dee797b60770ff96&utm_content=American+Express%7Cp%3AB%7Ca%3AAXCB%7C%5EB%7C%23D%7Cd%3AAmerican+Express+TP%7Cm%3AExact%7Cg%3ANA%7Csid%3A10449%7Cmcid%3A3560620");
+header("Location: https://www.gettingpersonal.co.uk/christmas-gifts");
 die();
 ?>
